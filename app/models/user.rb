fac98ad673a37794626f03eba9871c955072fc48
will <koyasu221b@gmail.com>
2015-09-22 09:04:58 +0800

    test new User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User
+    def initialize(attributes={})
+        @attributes = attributes
+    end
+
+    def id
+        @attributes[:id]
+    end
+
+    def name
+        @attributes[:name]
+    end
   
 end
--- test/user_test.rb
+++ test/user_test.rb
@@ -2,5 +2,10 @@ require "test_helper"
 require "user"
 
 class UserTest < ActiveSupport::TestCase
-  
+    def test_initialize_with_attributes
+        user = User.new(id: 1, name: "Marc")
+        assert_equal 1, user.id
+        assert_equal "Marc", user.name
+    end
+
 end
