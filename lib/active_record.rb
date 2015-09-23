3e592767256cea5a668d7a0afae8bb2a213bc6cd
will <koyasu221b@gmail.com>
2015-09-23 08:34:14 +0800

    refactoring the table name

--- lib/active_record.rb
+++ lib/active_record.rb
@@ -9,7 +9,7 @@ module ActiveRecord
         end
 
         def method_missing(name, *args)
-            columns = @@connection.columns("users")
+            columns = @@connection.columns(self.class.table_name)
             if columns.include?(name)
                 @attributes[name]
             else
@@ -19,9 +19,13 @@ module ActiveRecord
 
 
         def self.find(id)
-            attributes = @@connection.execute("SELECT * FROM users WHERE id = #{id.to_i} LIMIT 1").first
+            attributes = @@connection.execute("SELECT * FROM #{table_name} WHERE id = #{id.to_i} LIMIT 1").first
             #self.new attributes
             new attributes
+        end
+
+        def self.table_name
+            name.downcase + "s" # => "users"
         end
     end
 end
