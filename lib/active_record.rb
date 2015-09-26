213685b2dc0fe9496068e9105e008c3efcde2b30
will <koyasu221b@gmail.com>
2015-09-26 10:37:37 +0800

    implemented User.all

--- lib/active_record.rb
+++ lib/active_record.rb
@@ -8,6 +8,7 @@ module ActiveRecord
             @attributes = attributes
         end
 
+
         def method_missing(name, *args)
             columns = @@connection.columns(self.class.table_name)
             if columns.include?(name)
@@ -19,12 +20,31 @@ module ActiveRecord
 
 
         def self.find(id)
-            attributes = @@connection.execute("SELECT * FROM #{table_name} WHERE id = #{id.to_i} LIMIT 1").first
+            sql  = "SELECT * FROM #{table_name} WHERE id = #{id.to_i} LIMIT 1"
+            #attributes = @@connection.execute("SELECT * FROM #{table_name} WHERE id = #{id.to_i} LIMIT 1").first
             #self.new attributes
-            new attributes
+            return self.find_by_sql(sql).first
+        end
+
+        def self.all
+            sql = "SELECT * FROM #{table_name}"
+            return self.find_by_sql(sql)
+        end
+
+        def self.find_by_sql(sql)
+            @@connection.execute(sql).map do |attributes| # [atrributes, attributes]
+              new attributes
+            end
+            # objects = []
+            # @@connection.execute(sql).each do |attributes|
+            #     objects << (new attributes)
+            # end
+            # objects
         end
 
         def self.table_name
+            #User.name
+            #self.name
             name.downcase + "s" # => "users"
         end
     end
