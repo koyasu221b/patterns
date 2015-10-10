93a02c7b7307f022c505a56c954088cd88460d06
will <koyasu221b@gmail.com>
2015-10-10 08:26:40 +0800

    Implement after_action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   before_action :header
+  after_action :footer
   #HomeController.before_action :header
   def index
       response.write "hi from HomeController"
@@ -8,4 +9,9 @@ class HomeController < ApplicationController
   def header
       response.write "<h1>My App</h1>"
   end
+  
+  def footer 
+      response.write "<h1>My App footer</h1>"
+  end
+
 end
