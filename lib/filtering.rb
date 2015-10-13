9689c3e596bdf65d6af19f4ebfee924f1380ee23
will <koyasu221b@gmail.com>
2015-10-13 23:28:13 +0800

    Refactor Filtering using ActiveSupport

--- lib/filtering.rb
+++ lib/filtering.rb
@@ -1,32 +1,46 @@
+require 'active_support/concern'
+require 'active_support/callbacks'
+
 module Filtering
-    def self.included(base) # base == ActionController
-        base.extend ClassMethods
+    extend ActiveSupport::Concern
+    # def self.included(base) # base == ActionController
+    #     base.extend ClassMethods
+    # end
+    include ActiveSupport::Callbacks
+
+    included do
+        define_callbacks :process
     end
 
     module ClassMethods
       def before_action(method)
-          before_actions << method
+          #before_actions << method
+          set_callback :process, :before, method
       end
 
-      def before_actions
-          @before_actions ||= []
-      end
+      # def before_actions
+      #     @before_actions ||= []
+      # end
 
       def after_action(method)
-          after_actions << method
+          #after_actions << method
+          set_callback :process, :after, method
       end
 
-      def after_actions
-          @after_actions ||= []
-      end
+      # def after_actions
+      #     @after_actions ||= []
+      # end
 
     end
 
     def process(action_name)
-        puts "process on filtering"
-        self.class.before_actions.each {|method| send method}
-        super
-        self.class.after_actions.each {|method| send method}
+        # puts "process on filtering"
+        # self.class.before_actions.each {|method| send method}
+        # super
+        # self.class.after_actions.each {|method| send method}
+        run_callbacks :process do
+            super
+        end
     end
         
 end
